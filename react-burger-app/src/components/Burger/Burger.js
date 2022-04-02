11bbe6bf86caa1f7885be9aea9a3c374811c369c
davidobi <dbiggestdave@yahoo.com>
2022-04-02 08:23:54 +0000

    Add controls to the burger builder

--- react-burger-app/src/components/Burger/Burger.js
+++ react-burger-app/src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import './Burger.css';
 import BurgerIngerdient from './BurgerIngredient/BurgerIngerdient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
            return <BurgerIngerdient key={igKey + i} type ={igKey} />;
         });
-    });
+    }).reduce((arr,el) => {
+        return arr.concat(el)
+    },[]);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients.</p>
+    }    
     return (
         <div className='Burger'>
             <BurgerIngerdient type="bread-top" />
