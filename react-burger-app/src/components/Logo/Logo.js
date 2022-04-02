8c183ed5daa58354fa21102445f1d14857bc56de
davidobi <dbiggestdave@yahoo.com>
2022-04-02 16:18:45 +0000

    add side drawer for mobile nav

--- react-burger-app/src/components/Logo/Logo.js
+++ react-burger-app/src/components/Logo/Logo.js
@@ -2,8 +2,8 @@ import React from "react";
 import BurgerLogo from '../../assets/images/burger-logo.png';
 import './Logo.css'
 
-const logo = () => (
-    <div className="Logo">
+const logo = (props) => (
+    <div className="Logo" style={{height: props.height}} >
         <img src={BurgerLogo} alt="MyBurger" />
     </div>
 );
---
+++ react-burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import './SideDrawer.css';
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
+
+const sideDrawer = (props) => {
+
+    return (
+        <Aux>
+            <Backdrop show />
+            <div className="SideDrawer">
+                    <Logo height='11%' />
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+        
+    );
+};
+
+export default sideDrawer;
