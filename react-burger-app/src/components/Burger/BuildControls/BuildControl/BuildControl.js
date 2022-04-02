d7e11e996759c3f72fe8db224c6b952c8ef4cf48
davidobi <dbiggestdave@yahoo.com>
2022-04-02 09:17:49 +0000

    add disabled feature on the less button

--- react-burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ react-burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,20 @@ import './BuildControl.css';
 const buildControl = (props) => (
     <div className="BuildControl">
         <div className="Label">{props.label}</div>
-        <button className="Less">less</button>
-        <button className="More">more</button>
+        <button 
+            className="Less" 
+            onClick={props.removed} 
+            disabled={props.disabled} 
+        >
+            less
+        </button>
+        
+        <button 
+            className="More"  
+            onClick={props.added}
+        >
+                more
+        </button>
     </div>
 );
 
