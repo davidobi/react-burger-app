cac721bf97ceb65d3f4d4cda81467ad75161d5a3
davidobi <dbiggestdave@yahoo.com>
2022-04-02 10:21:53 +0000

    add order now button

--- react-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,11 +20,21 @@ class BurgerBuilder extends Component {
                 cheese: 0,
                 meat: 0
             },
-            totalPrice: 4
-        };
-         
-        
+            totalPrice: 4,
+            purchasable: false
+        };      
     }
+
+    updatePurchaseState (ingredients) {
+        
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey]
+        }).reduce(( sum, el) => {
+            return sum + el;
+        },0);
+        this.setState({purchasable: sum > 0});
+    }
+
     addIngredientHandler = (type) => {
             const oldCount = this.state.ingredients[type];
             const updatedCount = oldCount + 1;
@@ -35,7 +45,8 @@ class BurgerBuilder extends Component {
             const priceAddition = INGREDIENT_PRICES[type];
             const oldPrice = this.state.totalPrice;
             const newPrice = oldPrice + priceAddition;
-            this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+            this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+            this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -51,7 +62,8 @@ class BurgerBuilder extends Component {
             const priceReduction = INGREDIENT_PRICES[type];
             const oldPrice = this.state.totalPrice;
             const newPrice = oldPrice - priceReduction;
-            this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+            this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+            this.updatePurchaseState(updatedIngredients);
     }
 
     render () {
@@ -69,6 +81,7 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                     ingredientAdded = {this.addIngredientHandler}
                     ingredientRemoved = {this.removeIngredientHandler}
+                    purchasable = {this.state.purchasable}
                     disabled = {disabledInfo}
                     price = {this.state.totalPrice}
                 />
