3677dbdaec2f318471066bc19e52489ee9a37be5
davidobi <dbiggestdave@yahoo.com>
2022-04-02 15:03:22 +0000

    Add reusable navigation

--- react-burger-app/src/components/Navigation/Toolbar/Toolbar.js
+++ react-burger-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './Toolbar.css';
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
     <header className="Toolbar">
         <div>MENU</div>
         <Logo />
-        <nav>...</nav>
+        <nav>
+            <NavigationItems />
+        </nav>
     </header>
 );
 
