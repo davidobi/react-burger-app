5ce7a6f997d77b01dc6767b1a109645b14da2d5f
davidobi <dbiggestdave@yahoo.com>
2022-04-04 14:10:31 +0000

    add lifecycle hooks

--- react-burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ react-burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, {Component} from "react";
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients).map(igKey =>{
+class OrderSummary extends Component {
+//this could be a functional component. it doesnt have to be a class
+
+    UNSAFE_componentWillUpdate(){
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render () {
+        const ingredientsSummary = Object.keys(this.props.ingredients).map(igKey =>{
         return (
             <li key={igKey}>
                 <span style={{textTransform: 'capitalize'}}>{igKey}</span>: 
-                {props.ingredients[igKey]}
+                {this.props.ingredients[igKey]}
             </li>
             )
     });
-    
-    return (
-        
-        <Aux>
+        return (
+             <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientsSummary}
             </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    );
-};
+        );
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
